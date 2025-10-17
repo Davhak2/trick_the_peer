89ca45371a0a7a3e8b6090fb464cd7c2c6947079
Davhak2 <davidhakobyan014@gmail.com>
2025-10-17 08:01:12 +0400

    Improved a little

--- siggame.py
+++ siggame.py
@@ -10,18 +10,19 @@ class Trick:
 		self.attempts = 0
 		self.locked_until = 0
 		self.signal_attempts = 0
-		self.waiting_for_password = False
+		self.passwrd = False
+		self.flag = False
 		self.signals = {
-			signal.SIGINT:   "",
-			signal.SIGQUIT:  "",
-			signal.SIGTSTP:  "",
-			signal.SIGTERM:  "",
-			signal.SIGHUP:   "",
-			signal.SIGUSR1:  "",
-			signal.SIGUSR2:  "",
+			signal.SIGINT,
+			signal.SIGQUIT,
+			signal.SIGTSTP,
+			signal.SIGTERM,
+			signal.SIGHUP,
+			signal.SIGUSR1,
+			signal.SIGUSR2,
 		}
 
-		for sig, msg in self.signals.items():
+		for sig in self.signals:
 			try:
 				signal.signal(sig, self.handler)
 			except (OSError, RuntimeError, ValueError):
@@ -36,12 +37,22 @@ class Trick:
 
 	def clear_tab(self, show_message=True):
 		os.system('clear' if os.name == 'posix' else 'cls')
-		if show_message:
-			if self.signal_attempts >= 10:
+		effective_show = show_message and not self.hide_prompt
+		if self.hide_prompt:
+			self.hide_prompt = False
+		if effective_show:
+			if self.signal_attempts % 666 == 0:
+				print("Ara de zzvcir eli paroly \"ElKompsBacChemToxni\" a")
+			elif self.signal_attempts % 50 == 0:
+				print("Ara de asum em anasun erkir a")
+			elif self.signal_attempts % 15 == 0:
+				print("Ba urish vonc es ape?")
+			elif self.signal_attempts % 11 == 0:
+				print("Axjkeq jan vor hasneq 500 drami indzi dzen ktaq")
+			elif self.signal_attempts % 5 == 0:
 				print("Vay es el gitei qez ruchken a petq")
 			else:
 				print("Ujex qashi")
-
 
 	def mission_success(self):
 		self.clear_tab(show_message=False)
@@ -53,9 +64,9 @@ class Trick:
 		sys.exit(0)
 
 	def lockout(self):
-		self.locked_until = time.time() + 60
+		self.locked_until = time.time() + 30
 		self.clear_tab()
-		print("⏳ Too many attempts! Locked for 60 seconds...")
+		print("⏳ Too many attempts! Locked for 30 seconds...")
 		while time.time() < self.locked_until:
 			time.sleep(1)
 		self.attempts = 0
@@ -64,7 +75,7 @@ class Trick:
 
 	def default_loop(self):
 		while True:
-			if time.time() >= self.locked_until and not self.waiting_for_password:
+			if time.time() >= self.locked_until and not self.passwrd:
 				print("la", end="", flush=True)
 			time.sleep(0.08)
 
@@ -81,29 +92,30 @@ class Trick:
 					time.sleep(1)
 					continue
 				line = input("").strip()
-				print("Chkpav ynger jan")
 				self.clear_tab()
+				print("Chkpav ynger jan")
 			except EOFError:
 				if time.time() < self.locked_until:
 					print("⏳ Locked! Please wait...")
 					time.sleep(1)
 					continue
 				print("\nDavay de paroly asa")
-				self.waiting_for_password = True
+				self.passwrd = True
 				try:
 					password = input("").strip()
 					if password == self.secret:
 						self.mission_success()
 					else:
-						print("Chkpav ynger jan")
+						self.hide_prompt = True
 						self.clear_tab()
+						print("Chkpav ynger jan")
 						self.attempts += 1
 						if self.attempts % 5 == 0:
 							self.lockout()
 				except (EOFError, KeyboardInterrupt):
 					self.clear_tab(show_message=False)
 				finally:
-					self.waiting_for_password = False
+					self.passwrd = False
 			except KeyboardInterrupt:
 				if time.time() < self.locked_until:
 					print("⏳ Locked! Please wait...")
